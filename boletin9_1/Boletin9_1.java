fd5bd7d22126a36d1481db5150c585a181bc6083
Miguel Martinez Costa <mmartinezcosta@danielcastelao.org>
2019-11-15 17:42:48 +0100

    Bol9_1

---
+++ boletin9_1/Boletin9_1.java
@@ -0,0 +1,16 @@
+package boletin9_1;
+
+/**
+ *
+ * @author usuario
+ */
+public class Boletin9_1 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Calculo obx = new Calculo();
+        obx.calcular();
+
+    }
+
+}
