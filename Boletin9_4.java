6c08f90f1fb7c60377df4a92dde3e52b5ad0a7a6
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2019-11-19 10:42:40 +0100

    Update Boletin9_4.java

--- Boletin9_4.java
+++ Boletin9_4.java
@@ -10,7 +10,7 @@ public class Boletin9_4 {
     public static void main(String[] args) {
         // TODO code application logic here
    TablaMulti obx=new TablaMulti();
-    obx.Multiplicar();
+    obx.tablaMultiplicar();
     }
     
 }
