793183b7602d8e4eb7aa8d11891cc88ac9f2f7b2
Miguel Martinez Costa <56074037+Miguelmc5@users.noreply.github.com>
2019-11-26 08:47:22 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Sueldos obx1 = new Sueldos();
+        obx1.Calcular();
+    }
+}
